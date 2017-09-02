d6797b245cb65a87e55fb7a0f6307b7f891d2f5c
sgtp <3esk6j35#ESK^J#%>
2017-09-02 16:02:40 -0400

    refactor UT again, remove html assertions because they are considered constants and we don't need to test constants.

--- lists/tests.py
+++ lists/tests.py
@@ -5,23 +5,8 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_home_page(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-
-        # Make sure it's an html document
-        self.assertTrue(html.startswith('<html'))
-
-        # Make sure that shits in the title
-        self.assertIn('<title>To-Do-Lists</title>', html)
-
-        # Make sure it ends with html, but I don't think this is really needed
-        self.assertTrue(html.strip().endswith('/html>'))
 
         # Validate the correct template was used
         self.assertTemplateUsed(response, 'home.html')
