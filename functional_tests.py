3d9a29af517ea0765964b784e238dd40758d0083
sgtp <3esk6j35#ESK^J#%>
2017-09-03 23:58:37 -0400

    Add models and db stuff

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_table_for_row_text(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_todo_stuff_works(self):
         self.browser.get("http://localhost:8000")
 
@@ -32,27 +37,18 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_table_for_row_text('Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         time.sleep(2)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: use peacock feathers to make a flow', [row.text for row in rows])
-
-
-    def test_i_suck(self):
-        pass  # self.fail('Finish the test!')
+        self.check_table_for_row_text('use peacock feathers to make a fly')
+        self.check_table_for_row_text('Buy peacock feathers')
 
 
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -18,5 +17,22 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
+class ItemModelTest(TestCase):
 
+    def test_can_save_and_retrieve_data(self):
+        first_item = Item()
+        first_item.text = 'the first (ever) list item'
+        first_item.save()
 
+        second_item = Item()
+        second_item.text = 'the second list item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'the first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'the second list item')
