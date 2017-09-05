3f0b7e8320508d5adc06f726076bed2e7e741f01
sgtp <3esk6j35#ESK^J#%>
2017-09-04 21:12:06 -0400

    FT and UT good for ch 5, can add continious list via db connection

--- functional_tests.py
+++ functional_tests.py
@@ -29,15 +29,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item', 'YOU SHIT AINT GOT DAT SHIT')
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
-
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
-        self.check_table_for_row_text('Buy peacock feathers')
+        self.check_table_for_row_text('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -45,11 +40,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+        self.check_table_for_row_text('3: use peacock feathers to make a fly')
 
         time.sleep(2)
-        self.check_table_for_row_text('use peacock feathers to make a fly')
-        self.check_table_for_row_text('Buy peacock feathers')
-
+        self.check_table_for_row_text('1: Buy peacock feathers')
+        self.check_table_for_row_text('3: use peacock feathers to make a fly')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 
 def home_page(request):
     """Bla bla bla"""
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', 'fuck'),
-    })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
 
 
