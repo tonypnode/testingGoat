d00d51ed4237d2c2f3ad91459890815ce640839d
sgtp <3esk6j35#ESK^J#%>
2017-09-05 20:03:30 -0400

    Add FT for multiiuser lists

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
-import unittest
+
+MAX_WAIT = 5
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -13,12 +15,20 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_table_for_row_text(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows], ' I COULD NOT FIND ' + row_text + ' IN THE TABLE!!')
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    def test_todo_stuff_works(self):
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
 
         # page title mentions To-Do in the title
@@ -31,8 +41,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item', 'YOU SHIT AINT GOT DAT SHIT')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-        self.check_table_for_row_text('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -40,9 +49,46 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-        self.check_table_for_row_text('2: use peacock feathers to make a fly')
 
-        time.sleep(2)
-        self.check_table_for_row_text('1: Buy peacock feathers')
-        self.check_table_for_row_text('2: use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: use peacock feathers to make a fly')
+
+    def test_multiple_users_can_start_at_diff_urls(self):
+        # edith starts a new to do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # edith sees that her url is unique
+        edith_list_url = self.browser.current_url
+
+        self.assertRegex(edith_list_url, '/list/.+')
+
+        # New users comes along
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the page
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feather', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        #Fancis starts a new list
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+
+        # francis gets his own url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
