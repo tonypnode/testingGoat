79f519b70b50c03dd4ee27bfa01815546ffaaea2
sgtp <3esk6j35#ESK^J#%>
2017-09-10 08:44:51 -0400

    Add FT to validate style after post. Add base.html and extended to home and list templates

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -99,3 +99,11 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
